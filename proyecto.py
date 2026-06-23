66df183099b8469470705e5bc4d1b40080d5a831
grupo8-lcc <grupo8prog2@gmail.com>
2026-06-23 23:51:04 +0000

    prueba de la tabla

--- proyecto.py
+++ proyecto.py
@@ -111,9 +111,8 @@ def precios(precio_max:str, tabla:dict):
     
     for index in index_de_alquiler:
         for llave in tabla:
-            airbnbs[llave]=[]
-            airbnbs[llave].append(tabla[llave][index])
-    
+            airbnbs[llave]=tabla[llave][index]
+   
     return airbnbs
 
 
