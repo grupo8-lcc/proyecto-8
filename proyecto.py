ce32518d5d3a38c97cf46cb8db70dbc9a769b584
grupo8-lcc <grupo8prog2@gmail.com>
2026-06-25 14:41:15 +0000

    Se corrige grafica de torta

--- proyecto.py
+++ proyecto.py
@@ -247,7 +247,7 @@ def main():
 
     # Grafica de torta de cont_vecindarios
     vecindarios=cont_vecindarios(tabla)
-    fig, ax = plt.subplots(figsize=(0.5,0.5))
+    fig, ax = plt.subplots()
     ax.pie(vecindarios.values(), labels=vecindarios.keys(), autopct='%1.1f%%', labeldistance = 1.1, radius = 0.5)
     fig.tight_layout()
     ax.axis('equal')
