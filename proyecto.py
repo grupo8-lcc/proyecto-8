b943822bf1bc9b10235d7ca65a0ff43fd40f0c5f
grupo8-lcc <grupo8prog2@gmail.com>
2026-06-25 14:12:03 +0000

    Se completa el diseño de datos de algunas funciones

--- proyecto.py
+++ proyecto.py
@@ -248,7 +248,7 @@ def main():
     # Grafica de torta de cont_vecindarios
     vecindarios=cont_vecindarios(tabla)
     fig, ax = plt.subplots()
-    ax.pie(vecindarios.values(), labels=vecindarios.keys(), autopct='%1.1f%%')
+    ax.pie(vecindarios.values(), labels=vecindarios.keys(), autopct='%1.1f%%', pctdistance = 0.85, labeldistance = 1.1)
     ax.axis('equal')
     ax.set_title('Cantidad de valores por vecindario')
     st.pyplot(fig)
