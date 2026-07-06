3f41e965d5d63a68504215d776002ee4c3a488f7
grupo8-lcc <grupo8prog2@gmail.com>
2026-07-06 18:09:55 +0000

    modularizacion

--- proyecto.py
+++ proyecto.py
@@ -55,7 +55,6 @@ def procesam_dataset(f):
     #         tabla[lista_indice[x]].append(lista_datos[x])
     return tabla
 
-
 # PREGUNTA ESTATICA A RESOLVER: ¿QUE TIPOS DE HABITACIONES SON LAS MAS ALQUILADAS?
 
 # habitac_alquiladas:
@@ -66,8 +65,10 @@ def habitac_alquiladas(lista_habitaciones:list)->dict:
     habitac_alquiladas recibe la lista del dataset conteniendo los tipos de habitaciones,
     y devuelve un diccionario, que tiene la cantidad de cada tipo de habitacion.
     EJEMPLOS:
-        habitac_alquiladas(["Private room", "Hotel room", "Entire home/apt"]) -> {"Entire home/apt":1, "Private room":1, "Shared room":0, "Hotel room":1}
-        habitac_alquiladas(habitac alquiladas([]) -> {"Entire home/apt":0, "Private room":0, "Shared room":0, "Hotel room":0}) """
+        ->habitac_alquiladas(["Private room", "Hotel room", "Entire home/apt"]) == 
+        {"Entire home/apt":1, "Private room":1, "Shared room":0, "Hotel room":1}
+        ->habitac_alquiladas(habitac alquiladas([]) == 
+        {"Entire home/apt":0, "Private room":0, "Shared room":0, "Hotel room":0}) """
     
     dicc_habita={"Entire home/apt":0, "Private room":0, "Shared room":0, "Hotel room":0}
 
@@ -75,22 +76,42 @@ def habitac_alquiladas(lista_habitaciones:list)->dict:
         dicc_habita[habitaciones]+=1
 
     return dicc_habita
+def indice_alquiler(key:str, lista:list):
+    lista_indice=[]
+    i=0
+    if key=="price":
+        for precio in lista:
+            if precio and float(precio)<=float(precio_max):
+                lista_indice.append(i)
+            i+=1
+    elif key=="minimum_nights":
+        for noches in lista:
+            if int(noches)<=num:
+               lista_indice.append(i)
+            i+=1
+    return lista_indice
+
+
 
 # PREGUNTA: ¿Que alquileres tienen disponibles una cierta cantidad minima de noches?
 def Noches(num:int, dataset:dict)->dict:
     """Noches-> int, Dict{str:list[str]}
-    Noches recibe el numero que llega por el slider y recorre la seccion de minimum_nigths del diccionario
-    que representa el dataset, de ahi busca cuando el numero del slider coincide con la value guarda un indice en una lista
-    que seria el numero del airbnb que se esta guardando. Luego recorre esa lista accediendo a la latitud y longitud
-    de cada airbnb guardado con su posicion para guardarlas en otro diccionario el cual sera returneado"""
+    Noches recibe el numero que llega por el slider y recorre la seccion de 
+    minimum_nigths del diccionario que representa el dataset, de ahi busca
+    cuando el numero del slider coincide con la value guarda un indice en 
+    una lista que seria el numero del airbnb que se esta guardando. Luego 
+    recorre esa lista accediendo a la latitud y longitud de cada airbnb guardado 
+    con su posicion para guardarlas en otro diccionario el cual sera returneado"""
     dicc_alquileres={"latitude":[], "longitude":[]}
     index_de_alquiler=[]
     num=num
-    i=0
-    for noches in dataset["minimum_nights"]:
-        if int(noches)<=num:
-            index_de_alquiler.append(i)
-        i+=1
+
+    index_de_alquiler=indice_alquiler("minimum_nights", dataset["minimum_nights"])
+    #i=0
+    # for noches in dataset["minimum_nights"]:
+    #     if int(noches)<=num:
+    #         index_de_alquiler.append(i)
+    #     i+=1
     
     for x in index_de_alquiler:
         dicc_alquileres["latitude"].append(float(dataset["latitude"][x]))
@@ -101,13 +122,14 @@ def Noches(num:int, dataset:dict)->dict:
 # PREGUNTA: ¿Que alquileres hay por debajo de un precio seleccionado?
 def precios(precio_max:int, tabla:dict):
     """precios: str, dicc{str: list[str]} -> dicc{
-    precios, toma el valor ingresado por el usuario y el diccionario que representa el dataset
-    y devuelve un diccionario con los datos de todos los airbnb cuyo precio es menor o igual
-    al precio ingresado
+    precios, toma el valor ingresado por el usuario y el diccionario que 
+    representa el dataset y devuelve un diccionario con los datos de todos 
+    los airbnb cuyo precio es menor o igual al precio ingresado
     Ejemplo:
     (los tres puntos son el resto de campos del diccionario)
-    precios(600, {"price":[600, 400, 1000, 4000, 300], ...})== {...,"price":[600, 400, 300], ...}
-    precios(62, {"price":[600, 400, 1000, 4000, 300], ...})== {}"""
+    ->precios(600, {"price":[600, 400, 1000, 4000, 300], ...})== 
+    {...,"price":[600, 400, 300], ...}
+    ->precios(62, {"price":[600, 400, 1000, 4000, 300], ...})== {}"""
 
     # defino el diccionario y asigno las keys
     airbnbs={}
@@ -115,11 +137,12 @@ def precios(precio_max:int, tabla:dict):
         airbnbs[llave]=[]
     # guardo el indice de cada airbnb en el dataset que cumple lo que necesito
     index_de_alquiler=[]
-    i=0
-    for precio in tabla["price"]:
-        if precio and float(precio)<=float(precio_max):
-            index_de_alquiler.append(i)
-        i+=1
+    # i=0
+    # for precio in tabla["price"]:
+    #     if precio and float(precio)<=float(precio_max):
+    #         index_de_alquiler.append(i)
+    #     i+=1
+    index_de_alquiler=indice_alquiler("price", tabla["price"])
     # formo el diccionario con todos los airbnb que cumplen la condicion
     for index in index_de_alquiler:
         for llave in tabla:
@@ -130,12 +153,14 @@ def precios(precio_max:int, tabla:dict):
 # PREGUNTA: ¿Cuantos alquileres hay en cada vecindario?
 def cont_vecindarios(lista_vecindarios:list)->dict:
     """
-    representamos vecindarios como strings dentro de una list y la cantidad de sus apariciones en la lista como ints
+    representamos vecindarios como strings dentro de una list y la cantidad 
+    de sus apariciones en la lista como ints
     vecindario: str
     cantidad de apariciones de cada vecindarios: int
     cont_vecindarios: List(Str) -> Dict(str:Int)
-    cont_vecindarios recibe la lista del dataset de los vecindarios de cada airbnb
-    y devuelve un diccionario, cuyo key es cada vecindario y cada value es un contador de apariciones de dicho vecindario.
+    cont_vecindarios recibe la lista del dataset de los vecindarios de cada 
+    airbnb y devuelve un diccionario, cuyo key es cada vecindario y cada value 
+    es un contador de apariciones de dicho vecindario.
     EJEMPLO:
     cont_vecindario(["Copacabana", "Copacabana", "Copacabana", "Centro"]) == {"Copacabana":3,"Centro":1}
     cont_vecindario(["Copacabana"]) == {"Copacabana":1}
@@ -154,13 +179,19 @@ def cont_vecindarios(lista_vecindarios:list)->dict:
 # PREGUNTA: ¿Cuales alquileres son de determinado tipo de establecimiento?
 def clasif_props(tipo_de_prop:list[str], dataset:dict)->dict:
     """clasif_props-> list(str), Dict{str:list[str]}
-    clasif_props recibe el tipo de propiedad que llega por el checkbox y recorre la seccion de room_type del diccionario
-    que representa el dataset, de ahi busca cuando el tipo de prop. ingresado coincide con la value guarda un indice en 
-    una lista que seria el numero del airbnb que se esta guardando. Luego recorre esa lista accediendo a la latitud y 
-    longitud de cada airbnb guardado con su posicion para guardarlas en otro diccionario el cual sera returneado.
+    clasif_props recibe el tipo de propiedad que llega por el checkbox y recorre
+    la seccion de room_type del diccionario que representa el dataset, de ahi busca
+    cuando el tipo de prop. ingresado coincide con la value guarda un indice en 
+    una lista que seria el numero del airbnb que se esta guardando. Luego recorre
+    esa lista accediendo a la latitud y longitud de cada airbnb guardado con su
+    posicion para guardarlas en otro diccionario el cual sera returneado.
     EJEMPLO:
-    clasif_props(["Entire home/apt", "Private room"],{...,"room_type":["Entire home/apt","Entire home/apt","Private room"]},...) == {"latitude":[0, 90, 38], "longitude":[1, 20, 17]}
-    clasif_props(["Entire home/apt","Private room", "Shared room","Hotel room"],{...,"room_type":["Entire home/apt","Private room","Shared room", "Hotel room"],...})=={"latitude":[20, 0, 30, 15],"longitude":[0,21,45, 9]}"""
+    ->clasif_props(["Entire home/apt", "Private room"],
+    {...,"room_type":["Entire home/apt","Entire home/apt","Private room"]},...)
+    == {"latitude":[0, 90, 38], "longitude":[1, 20, 17]}
+    ->clasif_props(["Entire home/apt","Private room", "Shared room","Hotel room"],
+    {...,"room_type":["Entire home/apt","Private room","Shared room", "Hotel room"],...})
+    =={"latitude":[20, 0, 30, 15],"longitude":[0,21,45, 9]}"""
     dicc_alq_filtrados={"latitude":[], "longitude":[]}
     index_de_alquiler = {}
     for check in tipo_de_prop:
@@ -186,8 +217,7 @@ def main():
     """main es la funcion principal de nuestro programa, es la encargada
     del control del mismo."""
     tabla = {}
-    # tenemos problemas para leer el data set pero no sabemos cual ya distinguimos los dos casos problematicos
-    # y los resolvimos se puede ver en el archivo de prueba.txt pero sin embargo sigue tirando un error de index
+
     with open("dataset_airbnb.csv") as f:
         tabla = procesam_dataset(f)
     source = habitac_alquiladas(tabla["room_type"])
@@ -202,12 +232,15 @@ def main():
     ax.legend(title='Tipo de habitacion')
     st.pyplot(fig)
 
-    # Slider para elegir la cantidad de personas de la busqueda y mapa que muestra los alquileres
-    valor = st.slider("Minimo de noches que buscan alquilar", min_value=1, max_value=50, value=1)
+    # Slider para elegir la cantidad de personas de la busqueda y mapa que 
+    #muestra los alquileres
+    valor = st.slider("Minimo de noches que buscan alquilar", min_value=1, 
+    max_value=50, value=1)
     dicc_noches=Noches(valor, tabla)
     st.map(data=dicc_noches, latitude="latitude", longitude="longitude", zoom=11)
 
-    # Checkbox para elegir el tipo de propiedad buscada y mapa que muestra los alquileres
+    # Checkbox para elegir el tipo de propiedad buscada y mapa que muestra los 
+    #alquileres
     # "Entire home/apt"  | "Private room" | "Shared room" | "Hotel room"
     ent = st.checkbox("Entire home/apt")
     priv_room = st.checkbox("Private room")
@@ -222,27 +255,27 @@ def main():
         clasific.append("Shared room")
     if hotel_r:
         clasific.append("Hotel room")
-    # En caso de no tener ningun checkbox tildado, se opta por mostrar todas las ubicaciones de las propiedades del dataset
+    # En caso de no tener ningun checkbox tildado, se opta por mostrar 
+    #todas las ubicaciones de las propiedades del dataset
     if not clasific:
         clasific = ["Entire home/apt", "Private room", "Shared room", "Hotel room"]
     dicc_clasifprop = clasif_props(clasific, tabla)
     st.map(data=dicc_clasifprop, latitude="latitude", longitude="longitude", zoom=11)
     
     # Widget que toma un precio maximo
-    precio = st.number_input("Precio maximo que desea pagar:", value=0, placeholder="", step=1)
-    # Tabla donde se muestran los airbnb que cumplen con ese precio o menor
+    precio = st.number_input("Precio maximo que desea pagar:", value=0, 
+    placeholder="", step=1)
+    # Abajo estara el programa de la tabla
     dicc_precios = precios(precio, tabla)
-    # Mostrar como tabla
     # estara dividida en paginas ya que sino cuando hay muchas filas toda la pagina se traba
     # haciendola lenta y pesada
-    # Parametros para las paginas
     page_size = 30
-    page = st.number_input("Página", min_value=1, max_value=(len(dicc_precios["price"]) // page_size)+1, step=1)
+    page = st.number_input("Página", min_value=1,
+    max_value=(len(dicc_precios["price"]) // page_size)+1, step=1)
     start = (page - 1) * page_size
     end = start + page_size
     # se crean las subtablas que se van a mostrar en cada pagina
     subtabla={}
-    # k y v se refieren a keys y values
     for k, v in dicc_precios.items():
         # Tomar el slice de esa lista entre start y end
         valores_recortados = v[start:end]
@@ -254,7 +287,8 @@ def main():
     # Grafica de torta de cont_vecindarios
     vecindarios=cont_vecindarios(tabla["neighbourhood"])
     fig, ax = plt.subplots()
-    ax.pie(vecindarios.values(), labels=vecindarios.keys(), autopct='%1.1f%%', labeldistance = 1.1, radius = 0.5)
+    ax.pie(vecindarios.values(), labels=vecindarios.keys(),
+    autopct='%1.1f%%', labeldistance = 1.1, radius = 0.5)
     fig.tight_layout()
     ax.axis('equal')
     ax.set_title('Cantidad de valores por vecindario')
