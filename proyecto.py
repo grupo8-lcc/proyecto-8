51caa2a5acd55f1cca532fcadb45a24c51c0f26b
grupo8-lcc <grupo8prog2@gmail.com>
2026-07-07 17:48:15 +0000

    probando ultima pregunta

--- proyecto.py
+++ proyecto.py
@@ -349,7 +349,6 @@ def main():
     ["menos de un mes", "hace un mes", "ultimos 3 meses", "ultimos 6 meses"],)
     
     dicc_review=ultima_review(fecha_str, Intervalo, tabla)
-    st.write(dicc_review)
     st.map(data=dicc_review, latitude="latitude", longitude="longitude", zoom=11)
 
     return 0
